a4784b0ac7fb8fff8db2048a5b425f5e5618748c
dagmawiSolomon <02dagmawisolomon@gmail.com>
2024-10-22 05:49:47 +0300

    inital commit

---
+++ gui.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+
+window = tk.Tk()
+window.title("Spellchecker")
+window.geometry("500x250")  
+
+
+entry = tk.Text(window, height=8) 
+entry.pack(fill=tk.X, padx=10, pady=10)
+button = tk.Button(
+    window,
+    text="Spellcheck",
+    width=10,
+    height=2,
+)
+button.pack()
+window.mainloop()
