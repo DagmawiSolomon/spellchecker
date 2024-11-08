588703514c6234c2a1468586fc3d9cb73b980ce8
dagmawiSolomon <02dagmawisolomon@gmail.com>
2024-11-08 16:42:09 +0300

    feat: imporve the border colors of the txt area and button

--- gui.py
+++ gui.py
@@ -1,18 +1,23 @@
 import tkinter as tk
 
-
 window = tk.Tk()
+window.configure(bg="#090A0B")
 window.title("Spellchecker")
 window.geometry("550x250")  
 
+entry = tk.Text(window, height=8, borderwidth=0) 
+entry.configure(bg="#121416", fg="#F1E3E4")
+entry.pack(fill=tk.X)
 
-entry = tk.Text(window, height=8) 
-entry.pack(fill=tk.X, padx=10, pady=10)
+# Button widget
 button = tk.Button(
     window,
     text="Spellcheck",
-    width=10,
+    width=15,
     height=2,
+    bg="#E65F5C",
+    fg="#F1E3E4"
 )
-button.pack()
+button.pack(pady=10)
+
 window.mainloop()
