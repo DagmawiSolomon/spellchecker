0e68ad6c4fceaa4201bde1af505e106a43498861
dagmawiSolomon <02dagmawisolomon@gmail.com>
2024-11-08 16:52:57 +0300

    fix:remove borders from text and button

--- gui.py
+++ gui.py
@@ -5,19 +5,20 @@ window.configure(bg="#090A0B")
 window.title("Spellchecker")
 window.geometry("550x250")  
 
-entry = tk.Text(window, height=8, borderwidth=0) 
-entry.configure(bg="#121416", fg="#F1E3E4")
-entry.pack(fill=tk.X)
 
-# Button widget
+entry = tk.Text(window, height=8, borderwidth=0, highlightthickness=0) 
+entry.configure(bg="#121416", fg="#F1E3E4")
+entry.pack(fill=tk.X, padx=10, pady=10)
+
 button = tk.Button(
     window,
     text="Spellcheck",
     width=15,
     height=2,
-    bg="#E65F5C",
-    fg="#F1E3E4"
+    borderwidth=0,
+    highlightthickness=0
 )
-button.pack(pady=10)
+button.configure(bg="#E65F5C", fg="#F1E3E4")
+button.pack()
 
 window.mainloop()
