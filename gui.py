fb56eade5f32ecb3b98227f34d39cdc0e75957cb
dagmawiSolomon <02dagmawisolomon@gmail.com>
2024-11-08 16:56:17 +0300

    fix: disable window resizing

--- gui.py
+++ gui.py
@@ -5,7 +5,7 @@ window.configure(bg="#090A0B")
 window.title("Spellchecker")
 window.geometry("550x250")  
 
-
+window.resizable(False, False)
 entry = tk.Text(window, height=8, borderwidth=0, highlightthickness=0) 
 entry.configure(bg="#121416", fg="#F1E3E4")
 entry.pack(fill=tk.X, padx=10, pady=10)
