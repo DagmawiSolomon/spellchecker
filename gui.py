4615ca9da70c80d8fa0213e3c27ba180ab141178
dagmawiSolomon <02dagmawisolomon@gmail.com>
2024-11-12 15:59:45 +0300

    fix: remove the spellcheck button

--- gui.py
+++ gui.py
@@ -21,15 +21,5 @@ entry.pack(fill=tk.X, padx=10, pady=10)
 
 
 
-button = tk.Button(
-    window,
-    text="Spellcheck",
-    width=15,
-    height=2,
-    borderwidth=0,
-    highlightthickness=0
-)
-button.configure(bg="#E65F5C", fg="#F1E3E4")
-button.pack()
 
 window.mainloop()
