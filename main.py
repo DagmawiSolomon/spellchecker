57104584429a835bf2a40df35d8ac750c3bcbe5c
dagmawiSolomon <02dagmawisolomon@gmail.com>
2024-11-12 16:20:44 +0300

    style

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-from spellchecker import load_dictionary, wagner_fischer
+from spellchecker import SpellChecker
 user_input = input("Enter a word: ")
-dictionary = load_dictionary('words.txt')
-
-for word in dictionary:
-    if wagner_fischer(user_input, word) == 0:
-        print(word)
