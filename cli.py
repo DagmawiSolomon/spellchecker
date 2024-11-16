7f1b4c0ffb485b0b68605822acd39bbce8db74d9
dagmawiSolomon <02dagmawisolomon@gmail.com>
2024-11-16 05:09:05 +0300

    fix: Use a more descriptive message

--- cli.py
+++ cli.py
@@ -15,12 +15,18 @@ parser.add_argument(
 args = parser.parse_args()
 filepath = args.filepath
 
-words = []
-with open (filepath, 'r') as file:
-    words = file.read().split()
+def get_words(file_path):
+    with open (file_path, 'r') as file:
+        for line_num, line in enumerate(file, start=1):
+                words = line.split()
+                for word_num, word in enumerate(words, start=1):
+                    yield line_num, word_num, word
+        
     
 spellchecker = SpellChecker("words.txt")
-for i, word in enumerate(words):
-    print(f"{word}: {spellchecker.check(word)}")
-    
+for line_num, word_num, word in get_words(filepath):
+    suggestions = spellchecker.check(word)
+    if suggestions:
+        print(f"Misspelled word found at Line {line_num}, Word {word_num}: '{word}'. Possible corrections: {','.join(suggestions)}")
+   
     
