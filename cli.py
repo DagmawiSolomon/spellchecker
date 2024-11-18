2f47cce20a0ce64a238788df64cb93959d786b5b
dagmawiSolomon <02dagmawisolomon@gmail.com>
2024-11-18 04:59:51 +0300

    feat: add flags (--word and --file) to allow spellchecking a file or a word

--- cli.py
+++ cli.py
@@ -13,9 +13,15 @@ class SpellCheckerApp:
             description='A simple python spell checker',
         )
         parser.add_argument(
-            "filepath",
+            "--file",
             type=str,
-            help="The path to the file."
+            help = "Specify a file"
+        )
+        
+        parser.add_argument(
+            "--word",
+            type=str,
+            help = "Specify a word"
         )
         return parser.parse_args()
 
@@ -28,17 +34,35 @@ class SpellCheckerApp:
                 for word_num, word in enumerate(words, start=1):
                     yield line_num, word_num, word
 
-    def check_spelling(self, filepath):
+    def check_file(self, filepath):
         """Checks spelling in the provided file and suggests corrections."""
         for line_num, word_num, word in self.get_words(filepath):
             suggestions = self.spellchecker.check(word)
             if suggestions:
                 print(f"Misspelled word found at Line {line_num}, Word {word_num}: '{word}'. "
                       f"Possible corrections: {', '.join(suggestions)}")
+                
+    def check_word(self, word):
+        """Checks spelling of the provided word and suggests corrections."""
+        suggestions = self.spellchecker.check(word)
+        if suggestions:
+            print(f"Possible corrections for the word '{word}': {', '.join(suggestions)}")
+        else:
+            print(f"The word '{word}' appears to be spelled correctly.")
+        
+        
 
     def run(self):
         args = self.parse_arguments()
-        self.check_spelling(args.filepath)
+        if args.file:
+            self.check_file(args.file)
+        elif args.word:
+            self.check_word(args.word)
+        else:
+            print("You must specify either a file or a word to spell check")
+           
+
+            
 
 
 if __name__ == "__main__":
