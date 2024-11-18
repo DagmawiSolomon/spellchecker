4fa3b85bd7327dd08ccd4a4261025a7d068a70ce
dagmawiSolomon <02dagmawisolomon@gmail.com>
2024-11-18 04:31:08 +0300

    feat: refactor code to use classes

--- cli.py
+++ cli.py
@@ -1,32 +1,46 @@
 import argparse
 from spellchecker import SpellChecker
 
-parser = argparse.ArgumentParser(
-                    prog='SpellChecker',
-                    description='A simple python spell checker',
-)
 
-parser.add_argument(
-        "filepath",
-        type=str,
-        help="The path to the file."
-)
+class SpellCheckerApp:
+    def __init__(self, dictionary_path):
+        self.spellchecker = SpellChecker(dictionary_path)
 
-args = parser.parse_args()
-filepath = args.filepath
+    @staticmethod
+    def parse_arguments():
+        parser = argparse.ArgumentParser(
+            prog='SpellChecker',
+            description='A simple python spell checker',
+        )
+        parser.add_argument(
+            "filepath",
+            type=str,
+            help="The path to the file."
+        )
+        return parser.parse_args()
 
-def get_words(file_path):
-    with open (file_path, 'r') as file:
-        for line_num, line in enumerate(file, start=1):
+    @staticmethod
+    def get_words(file_path):
+        """Generator to yield words from the file along with their positions."""
+        with open(file_path, 'r') as file:
+            for line_num, line in enumerate(file, start=1):
                 words = line.split()
                 for word_num, word in enumerate(words, start=1):
                     yield line_num, word_num, word
-        
-    
-spellchecker = SpellChecker("words.txt")
-for line_num, word_num, word in get_words(filepath):
-    suggestions = spellchecker.check(word)
-    if suggestions:
-        print(f"Misspelled word found at Line {line_num}, Word {word_num}: '{word}'. Possible corrections: {','.join(suggestions)}")
-   
-    
+
+    def check_spelling(self, filepath):
+        """Checks spelling in the provided file and suggests corrections."""
+        for line_num, word_num, word in self.get_words(filepath):
+            suggestions = self.spellchecker.check(word)
+            if suggestions:
+                print(f"Misspelled word found at Line {line_num}, Word {word_num}: '{word}'. "
+                      f"Possible corrections: {', '.join(suggestions)}")
+
+    def run(self):
+        args = self.parse_arguments()
+        self.check_spelling(args.filepath)
+
+
+if __name__ == "__main__":
+    app = SpellCheckerApp(dictionary_path="words.txt")
+    app.run()
