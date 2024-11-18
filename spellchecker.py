bf20e892b2c56fe225d6994d4f70a78171241180
dagmawiSolomon <02dagmawisolomon@gmail.com>
2024-11-18 05:35:05 +0300

    style

--- spellchecker.py
+++ spellchecker.py
@@ -1,32 +1,38 @@
 
 class SpellChecker:
-    
+
     def __init__(self, filepath):
         self.filepath = filepath
         self.words = self.load_dictionary(filepath)
     def wagner_fischer(self, str1: str, str2: str) -> int:
-        """Calculates the Levenshtein edit distance between two strings using the Wagner-Fischer algorithm.
-        
-        The Levenshtein distance is a metric for measuring the difference between two sequences. 
-        It represents the minimum number of single-character edits (insertions, deletions, or substitutions) 
+
+        """
+        Calculates the Levenshtein edit distance between two strings
+        using the Wagner-Fischer algorithm.
+
+        The Levenshtein distance is a metric for measuring the
+        difference between two sequences.
+        It represents the minimum number of single-character edits
+        (insertions, deletions, or substitutions)
         required to transform one string into the other.
-        
+
         Args:
             str1 (str): The first input string.
             str2 (str): The second input string.
-        
+
         Returns:
-            int: The edit distance between the two strings, indicating the minimum number of edits needed.
+            int: The edit distance between the two strings,
+            indicating the minimum number of edits needed.
         """
         m, n = len(str1), len(str2)
-        
+
         # Initialize a 2D array with dimensions (n+1) x (m+1) for storing edit distances
         D = [[0] * (m+1) for _ in range(n+1)]
-        
+
         # Initialize the first column of the array (edit distance from str1 to an empty str2)
         for i in range(n+1):
             D[i][0] = i
-        
+
         # Initialize the first row of the array (edit distance from an empty str1 to str2)
         for j in range(m+1):
             D[0][j] = j
